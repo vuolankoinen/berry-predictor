a43d181a49c391ad11eded0b6d419b4df4476736
vuolankoinen <matti.palomaki@helsinki.fi>
2019-07-12 18:30:50 +0300

    Change weather station to bypass url-encoding trouble

--- src/get-weather-data.py
+++ src/get-weather-data.py
@@ -1,14 +1,23 @@
-import urllib.request as urlr # https://docs.python.org/3.7/library/urllib.request.html#module-urllib.request
+import urllib.request # https://docs.python.org/3.7/library/urllib.request.html#module-urllib.request
+import urllib.parse
 import sys
+import time
 
-starttime = '1999-06-20T10:30:00Z'
-endtime = '1999-06-24T10:30:00Z'
-req = '/wfs?request=getFeature&storedquery_id=fmi::observations::weather::multipointcoverage&place=Helsinki&starttime=' + starttime  + '&endtime=' + endtime + '&timestep=120&parameters=r_1h,t2m,ws_10min,wg_10min,wd_10min,rh,td,p_sea,vis,n_man'
+begin = 'http://data.fmi.fi/fmi-apikey/' + sys.argv[1]
+starttime = '2018-09-01T01:00:00Z'
+endtime = '2019-06-24T10:30:00Z'
+#req = begin + '/wfs?request=getFeature&storedquery_id=fmi::observations::weather::multipointcoverage&place=Helsinki&starttime=' + starttime  + '&endtime=' + endtime + '&timestep=120&parameters=r_1h,t2m,ws_10min,wg_10min,wd_10min,rh,td,p_sea,vis,n_man'
 
-req2 = 'http://data.fmi.fi/fmi-apikey/5a8163a1-6462-419d-b0aa-66d9e27ed373/wfs?request=getFeature&storedquery_id=fmi::observations::weather::monthly::multipointcoverage&place=Rovaniemi&starttime=2005-05-05T01:00:00Z&endtime=2013-05-05T01:00:00Z&'
+req = begin + '/wfs?request=getFeature&storedquery_id=fmi::observations::weather::monthly::multipointcoverage&place=Kittil' + urllib.parse.quote('ä') + '&Starttime=' + starttime + '&endtime=' + endtime + '&'
+
+req = begin + '/wfs?request=getFeature&storedquery_id=fmi::observations::weather::monthly::multipointcoverage&place=Pello&Starttime=' + starttime + '&endtime=' + endtime + '&'
+
 # Korkeintaan 10 vuoden kuukausikeskiarvot kerralla.
+# req = list(urllib.parse.urlsplit(req))
+# req[3] = urllib.parse.quote(req[3])
+# req = urllib.parse.urlunsplit(req)
 
-print(('http://data.fmi.fi/fmi-apikey/' + sys.argv[1] + req), '\n')
-
-data = urlr.urlopen('http://data.fmi.fi/fmi-apikey/' + sys.argv[1] + req).read()
+# print(time.strftime('%Y-%m-%dT01:00:00Z'))
+print(req)
+data = urllib.request.urlopen(req)
 print(data)

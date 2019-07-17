ee0f266282f28167574c841168e58a3a4f4171cf
vuolankoinen <matti.palomaki@helsinki.fi>
2019-07-17 22:22:14 +0300

    Some work on loading the recent data

--- data/get-weather-data.py
+++ data/get-weather-data.py
@@ -2,22 +2,62 @@ import urllib.request # https://docs.python.org/3.7/library/urllib.request.html#
 import urllib.parse
 import sys
 import time
+import pandas as pd
+import xml.etree.ElementTree as et
+
+# Seuraavaksi: senhetkisen pvm:n ottaminen huomioon datan lataamisessa.
+# pvm:n mukaan data ennusteita varten joko ladatusta tai keskiarvoista.
+# sitten naista muodostetun uusimman datan tallentaminen
 
 begin = 'http://data.fmi.fi/fmi-apikey/' + sys.argv[1]
-starttime = '2018-09-01T01:00:00Z'
-endtime = '2019-06-24T10:30:00Z'
-#req = begin + '/wfs?request=getFeature&storedquery_id=fmi::observations::weather::multipointcoverage&place=Helsinki&starttime=' + starttime  + '&endtime=' + endtime + '&timestep=120&parameters=r_1h,t2m,ws_10min,wg_10min,wd_10min,rh,td,p_sea,vis,n_man'
+# starttime = '2018-09-01T01:00:00Z'
+# endtime = '2019-06-24T10:30:00Z'
+y = int(time.strftime('%Y'))
+m = int(time.strftime('%m'))
+starttime = time.strftime('{}-06-01T01:00:00Z'.format(y-1))
+endtime = time.strftime('{}-{:02d}-01T01:00:00Z'.format(y,m))
+print(starttime)
+print(endtime)
+# req = begin + '/wfs?request=getFeature&storedquery_id=fmi::observations::weather::multipointcoverage&place=Helsinki&starttime=' + starttime  + '&endtime=' + endtime + '&timestep=120&parameters=r_1h,t2m,ws_10min,wg_10min,wd_10min,rh,td,p_sea,vis,n_man'
+
+# print(time.strftime('%Y-%m-%dT01:00:00Z', time.gmtime(time.time())))
+# print(time.strftime('%Y-%m-%dT01:00:00Z', time.gmtime(time.time()-60*60*24)))
 
 req = begin + '/wfs?request=getFeature&storedquery_id=fmi::observations::weather::monthly::multipointcoverage&place=Kittil' + urllib.parse.quote('ä') + '&Starttime=' + starttime + '&endtime=' + endtime + '&'
 
-req = begin + '/wfs?request=getFeature&storedquery_id=fmi::observations::weather::monthly::multipointcoverage&place=Pello&Starttime=' + starttime + '&endtime=' + endtime + '&'
-
 # Korkeintaan 10 vuoden kuukausikeskiarvot kerralla.
-# req = list(urllib.parse.urlsplit(req))
-# req[3] = urllib.parse.quote(req[3])
-# req = urllib.parse.urlunsplit(req)
 
 # print(time.strftime('%Y-%m-%dT01:00:00Z'))
-print(req)
-data = urllib.request.urlopen(req)
-print(data)
+
+# The index picks the data from the months that have already passed this year,
+# interlacing the temperature and rain measurements.
+# The indexes 0-8 are the temperatures of the ongoing year.
+# The indexes 9-15 are the temperatures of the previous year.
+# The indexes 16-24 are the rain amounts of the ongoing year.
+# The indexes 25-31 are the rain amounts of the previous year.
+month_now = int(time.strftime('%m'))
+ind = [16, 0, 17, 1, 18, 2, 19, 3, 20, 4, 21, 5, 22, 6, 23, 7, 24, 8][0:(2*month_now-2)]
+
+data = urllib.request.urlopen(req).read().decode("utf-8")
+
+file = open('recent.dat', 'w')
+file.write(data)
+file.close()
+
+root = et.fromstring(data)
+
+prediction_input = pd.read_csv('column-means.csv')
+prediction_input = prediction_input.set_index(prediction_input.iloc[:, 0])
+prediction_input = prediction_input.iloc[:, 1]
+print(prediction_input)
+
+for br in root.iter():
+    if br.tag == '{http://www.opengis.net/gml/3.2}doubleOrNilReasonTupleList':
+        da = list(map(float, br.text.split()))
+
+print(da)
+
+for uus, i in enumerate(ind):
+    prediction_input[i] = da[uus]
+
+print(prediction_input)
--- src/nnet/train-predictor.py
+++ src/nnet/train-predictor.py
@@ -12,18 +12,24 @@ years = list(range(1990, 2019))
 data = pd.read_csv('../../data/Pello-kk-1989-2019-kk.csv')
 data.columns = ['year', 'month', 'x1', 'x2', 'x3', 'rain', 'T']
 data = data[['year', 'month', 'rain', 'T']]
-# Variables are rain (R) and temperature (T) for each month (1-12):
-X_Lappi = pd.DataFrame(columns=list(map(lambda x : x+"T", map(str, range(1,13)))) + list(map(lambda x : x+"R", map(str, range(1,13)))))
+
+# Variables are rain and temperature for the preceeding months and the berry sales of the preceeding months.
+X_Lappi = pd.DataFrame(columns=list(map(lambda x : x+"T", map(str, range(1,17)))) + list(map(lambda x : x+"R", map(str, range(1,17)))))
 
 for ye in years:
     new = []
-    for mo in range(1,13):
+    for mo in range(1,9):
         new += list(data[data['year'] == ye][data['month'] == mo][['T']].iloc[0])
-    for mo in range(1,13):
+    for mo in range(5,13):
+        new += list(data[data['year'] == ye-1][data['month'] == mo][['T']].iloc[0])
+    for mo in range(1,9):
         new += list(data[data['year'] == ye][data['month'] == mo][['rain']].iloc[0])
+    for mo in range(5,13):
+        new += list(data[data['year'] == ye-1][data['month'] == mo][['rain']].iloc[0])
     X_Lappi.loc[ye-1990] = new
 X_Lappi['year'] = years
 X_Lappi.set_index('year', inplace = True)
+print(X_Lappi)
 X_Lappi.fillna(X_Lappi.mean(), inplace = True) # Fill in missing values with means.
 Y = pd.read_csv('../../data/berries-sales-volumes.csv')
 Y.set_index('year', inplace = True)
@@ -32,29 +38,34 @@ Y = Y[years]
 mean_Y = np.mean(Y)
 Y = Y / mean_Y
 
+# X_Lappi.mean().to_csv('../../data/column-means.csv')
+F = X_Lappi.mean()
+print(F)
+F = F.to_frame()
+F.to_csv('../../data/column-means.csv')
+print('***')
+print(F)
+
 ####
 # Build the neural net
 # First let us do this blindly and just throw all the data at the net without any thought towards what might not be relevant.
-
 net = Sequential()
-net.add(Dense(12, input_dim = 24))
+net.add(Dense(16, input_dim = 32))
 net.add(Activation(activation = 'sigmoid'))
-net.add(Dense(10))
+net.add(Dense(12))
 net.add(Activation(activation = 'relu'))
 net.add(Dense(1))
 net.add(Activation(activation = 'linear'))
 net.compile(optimizer = 'AdaGrad', loss = 'mse')
 
 # Train the neural net
-net.fit(X_Lappi, Y, epochs = 2000)
+net.fit(X_Lappi, Y, epochs = 1000)
 net.summary()
 
 # Save the net
 net.save("Lingonberry-Lapland.net")
 
 # Print the results on training data
-
-print(np.array(X_Lappi.loc[1990:1990]))
-
 for ye in years:
     print("Year ", ye, ": prediction ", np.round(net.predict(np.array(X_Lappi.loc[ye:ye]))[0][0]*mean_Y, decimals = 1), ", true amount ", Y[ye]*mean_Y, ".", sep="")
+

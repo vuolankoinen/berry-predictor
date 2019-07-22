60c0aacd639d50945daa6e0bad756bb3b0c65726
vuolankoinen <matti.palomaki@helsinki.fi>
2019-07-22 18:22:15 +0300

    Server test

--- src/app.py
+++ src/app.py
@@ -8,11 +8,13 @@ class Server(BaseHTTPRequestHandler):
         self.send_response(200)
         self.end_headers()
         # prediction_text = 'Berry prediction:\nAt this time, the predicted amount of lingonberries to reach sales in Lapland is {} tons.'.format(pred_lingonberries()[0][0])
-        #self.wfile.write(prediction_text.encode())
+        # self.wfile.write(prediction_text.encode())
+        self.wfile.write(list_dirs().encode())
         self.wfile.write((os.getcwd() + '\n').encode())
         self.wfile.write(b'Toimii jotenkin.')
         os.chdir('..')
         self.wfile.write((os.getcwd() + '\n').encode())
+        self.wfile.write(list_dirs().encode())
 
 def list_files():
     res = ""
@@ -22,11 +24,20 @@ def list_files():
     #for r, d, f in os.walk("../data"):
     return res
 
+def list_dirs():
+    res = ""
+    # for root, dirs, files in os.walk("."):
+    root, dirs, files = os.walk(".")
+    for d in dirs:
+        res = res + "\n" + d
+    #for r, d, f in os.walk("../data"):
+    return res
+
 def start_server():
     HTTPServer(('', int(os.environ['PORT'])), Server).serve_forever()
 
 def pred_lingonberries():
-    net = load_model('nnet/Lingonberry-Lapland.net')
+    net = load_model('../src/nnet/Lingonberry-Lapland.net')
     recent = pd.read_csv('../data/recent.csv')
     rec = pd.DataFrame(columns=range(0, len(recent)))
     rec.loc[0] = list(recent.iloc[:, 1])

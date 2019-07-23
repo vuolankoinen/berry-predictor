ed1c5d38a1051e63a542a86eaf02e2dc7cfcb309
vuolankoinen <matti.palomaki@helsinki.fi>
2019-07-23 04:06:05 +0300

    Server test

--- src/app.py
+++ src/app.py
@@ -10,19 +10,15 @@ class Server(BaseHTTPRequestHandler):
         prediction_test = ""
         try:
             net = load_model(os.getcwd() + '/src/nnet/Lingonberry-Lapland.net')
-            prediction_text = 'Berry prediction:\nAt this time, the predicted amount of lingonberries to reach sales in Lapland is {} tons.'.format(pred_lingonberries()[0][0])
+            prediction_text = '<p>Berry prediction:<br>At this time, the predicted amount of lingonberries to reach sales in Lapland is {} tons.</p>'.format(pred_lingonberries()[0][0])
         except Exception as e:
             prediction_text = prediction_text + '<p>File not found at getcwd/src/nnet: ' + str(e) + '</p>'
         self.wfile.write(prediction_text.encode())
-        self.wfile.write(("<p>listdir get:" + str(os.listdir(os.getcwd())) + "</p>").encode())
-        self.wfile.write(("<p>listdir get:" + str(os.listdir(os.getcwd() + '/src')) + "</p>").encode())
-        self.wfile.write(("<p>listdir get:" + str(os.listdir(os.getcwd() + '/src/nnet')) + "</p>").encode())
-        os.chdir('./src')
-        self.wfile.write(('<p>' + os.getcwd() + '</p>').encode())
-        # self.wfile.write(list_dirs().encode())
-        self.wfile.write(("<p>listdir .: " + str(os.listdir('.')) + "</p>").encode())
-        self.wfile.write(("<p>listdir ..:" + str(os.listdir('..')) + "</p>").encode())
-        self.wfile.write(("<p>listdir get:" + str(os.listdir(os.getcwd())) + "</p>").encode())
+        # self.wfile.write(("<p>listdir get:" + str(os.listdir(os.getcwd())) + "</p>").encode())
+        # self.wfile.write(("<p>listdir get:" + str(os.listdir(os.getcwd() + '/src')) + "</p>").encode())
+        # self.wfile.write(("<p>listdir get:" + str(os.listdir(os.getcwd() + '/src/nnet')) + "</p>").encode())
+        # self.wfile.write(('<p>' + os.getcwd() + '</p>').encode())
+        # self.wfile.write(("<p>listdir get:" + str(os.listdir(os.getcwd())) + "</p>").encode())
 
 def start_server():
     HTTPServer(('', int(os.environ['PORT'])), Server).serve_forever()
